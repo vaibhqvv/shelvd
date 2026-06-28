98fddeb46d0bf219b63d6b59c72306205b9f7250
vaibhqvv <voidsyntaxes@gmail.com>
2026-06-28 22:15:50 +0530

    add deployment and repository cleanup files

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
+app = Flask(__name__)
+
+def get_book_titles():
+    return sorted(list(pt.index))
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+
+@app.route('/recommend')
+@app.route('/recommend/')
+def recommend_ui():
+    return render_template('recommend.html', book_titles=get_book_titles())
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input', '').strip()
+    matches = np.where(pt.index == user_input)[0]
+
+    if len(matches) == 0:
+        return render_template(
+            'recommend.html',
+            book_titles=get_book_titles(),
+            query=user_input,
+            error='That title is not in the recommendation dataset yet. Try choosing one from the suggestions.',
+        )
+
+    index = matches[0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    return render_template('recommend.html', data=data, book_titles=get_book_titles(), query=user_input)
+
+if __name__ == '__main__':
+    app.run(debug=True)
